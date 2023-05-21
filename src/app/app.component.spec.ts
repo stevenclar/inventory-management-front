53732d4c0a74df3b06a64087e9192b795405c83e
Deivid Steven Gonzalez <deividclar@gmail.com>
2023-05-21 17:18:24 -0500

    Config tailwindcss and create Readme

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'podcaster'`, () => {
+  it(`should have as title 'Inventory Management'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('podcaster');
+    expect(app.title).toEqual('Inventory Management');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('podcaster app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Inventory Management app is running!');
   });
 });
