d03e9de38410b8043246204619528e83a95ac8ca
Deivid Steven Gonzalez <deividclar@gmail.com>
2023-05-21 20:00:19 -0500

    Get companies from backend and add filter functionality

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,56 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import { TranslateService } from '@ngx-translate/core';
 import Company from 'src/app/core/interfaces/company';
+import { CompanyService } from 'src/app/core/services/company/company.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   searchValue: string = '';
   faSearch = faSearch;
-  companies: Company[] = [
-    {
-      nit: '1',
-      name: 'Company 1',
-      description: 'Aliquip sunt nostrud nostrud sit aute eiusmod ad. 1',
-      address: 'Address 1',
-      phone: 'Phone 1',
-    },
-    {
-      nit: '2',
-      name: 'Company 2',
-      description: 'Dolor duis eu minim ad sint proident quis occaecat est. 2',
-      address: 'Address 2',
-      phone: 'Phone 2',
-    },
-    {
-      nit: '3',
-      name: 'Company 3',
-      description: 'Ad ullamco sint do esse ea anim do. 3',
-      address: 'Address 3',
-      phone: 'Phone 3',
-    },
-    {
-      nit: '4',
-      name: 'Company 4',
-      description:
-        'Dolor amet anim voluptate quis amet in fugiat sunt enim qui dolore dolore ea do. 4',
-      address: 'Address 4',
-      phone: 'Phone 4',
-    },
-    {
-      nit: '5',
-      name: 'Company 5',
-      description:
-        'Quis magna aliquip esse esse ad elit anim magna velit laboris sunt. 5',
-      address: 'Address 5',
-      phone: 'Phone 5',
-    },
-  ];
+  companies: Company[] = [];
 
-  constructor(private readonly translateService: TranslateService) {}
+  constructor(
+    private readonly translateService: TranslateService,
+    private readonly companyService: CompanyService
+  ) {}
+
+  ngOnInit(): void {
+    this.companyService.getCompanies().subscribe((result: any) => {
+      this.companies = result?.data as Company[];
+    });
+  }
 
   public get searchPlaceholder(): string {
     return this.translateService.instant('home.searchPlaceholder');
